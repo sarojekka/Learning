be7fccdeb15625c7ffccd62b87e128ecd872a13a
Saroj Ekka <saroj.ekka@hpe.com>
2023-11-15 10:52:32 -0800

    Fixed issue of Amplify deployment

--- React/Sample1/src/index.js
+++ React/Sample1/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./components/app";
+import App from "./components/App";
 
 const container = document.getElementById("app");
 ReactDOM.hydrateRoot(container, <App />);
